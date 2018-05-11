32630e75025917cfb3bee9cb52e89996ee787e95
swalker1 <swalker1@underarmour.com>
2018-05-11 17:52:44 -0400

    Objects objects objects

---
+++ src/objects.js
@@ -0,0 +1,12 @@
+var die = {
+    size: 4,
+    count: 1,
+    roll: function(dieSize) {
+        var result = Math.ceil(dieSize * Math.random());
+        return result;
+    }
+
+};
+
+console.log(die);
+console.log(die.roll(6));
