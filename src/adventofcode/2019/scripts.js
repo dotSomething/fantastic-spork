9c62387e11fb747da1aa961b8b6ae84b76be607d
dotSomething <17045972+dotSomething@users.noreply.github.com>
2019-12-12 14:29:33 -0500

    AOC Day 2, Part 2

--- src/adventofcode/2019/scripts.js
+++ src/adventofcode/2019/scripts.js
@@ -4,53 +4,64 @@ function calculateFuel(masses) {
 	}, 0)
 }
 
-function resetGravityAssist(opCodes, operation, noun = 12, verb = 2) {
-	(function restoreState() {
-		opCodes[1] = 12;
-		opCodes[2] = 2;
-	})();
+function restoreState(opCodes, noun, verb) {
+	opCodes[1] = noun;
+	opCodes[2] = verb;
 
-	function reset() {
-		for (let i = 0; i < opCodes.length; i += 4) {
-			const opCode = opCodes[i];
-			const position_1 = opCodes[opCodes[i + 1]];
-			const position_2 = opCodes[opCodes[i + 2]];
-			const outputIndex = opCodes[i + 3];
-
-			if (opCode === 1) {
-				opCodes[outputIndex] = position_1 + position_2;
-			} else if (opCode === 2) {
-				opCodes[outputIndex] = position_1 * position_2;
-			} else if (opCode === 99) {
-				return opCodes[0];
-			} else {
-				console.error(`Invalid code:  ${opCode}`);
-			}
-		}
-	}
-
-	function initiate() {
-		let result;
-		opCodes.forEach(code => {
-			let output = code * noun + verb;
-			if (output === 19690720) {
-				result = code;
-			}
-		});
-		return result;
-	}
-
-	function getResults(params) {
-		if (operation == 'reset') {
-			reset();
-		} else if (operation == 'initiate') {
-			initiate();
-		}
-	}
-	getResults();
 }
+
+function gravityAssist([...opCodes], noun, verb) {
+	restoreState(opCodes, noun, verb);
+
+	for (let i = 0; i < opCodes.length; i += 4) {
+		const opCode = opCodes[i];
+		const position_1 = opCodes[opCodes[i + 1]];
+		const position_2 = opCodes[opCodes[i + 2]];
+		const outputIndex = opCodes[i + 3];
+
+		if (opCode === 1) {
+			opCodes[outputIndex] = position_1 + position_2;
+		} else if (opCode === 2) {
+			opCodes[outputIndex] = position_1 * position_2;
+		} else if (opCode === 99) {
+			return opCodes[0];
+		} else {
+			console.error(`Invalid code:  ${opCode}`);
+			return;
+		}
+	}
+}
+
+function initGravityAssistArch(opCodes, noun, verb, output) {
+	restoreState(opCodes, noun, verb);
+
+	for (let i = 0; i < opCodes.length; i++) {
+		const formNoun = opCodes[i];
+		const formVerb = opCodes[opCodes[i + 1]];
+		const analysis = 100 * formNoun + formVerb;
+		if (analysis == output) {
+			let results = [noun, verb]
+			return results;
+		}
+		// console.log(`100 * ${formNoun} + ${formVerb} = ${analysis}`);
+	}
+}
+
+function initGravityAssist(opCodes, output) {
+
+	for (let i = 0; i < 100; i++) {
+
+		for (let j = 0; j < 100; j++) {
+			if(gravityAssist([...opCodes], i, j) == output) {
+				return 100 * i + j;
+			}
+		}
+	}
+}
+
 
 module.exports = {
 	calculateFuel,
-	resetGravityAssist
+	gravityAssist,
+	initGravityAssist
 }
