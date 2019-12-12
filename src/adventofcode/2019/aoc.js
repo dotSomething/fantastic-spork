9785c422a37ebc945c2f175f5a98ca8a3bf6836e
dotSomething <17045972+dotSomething@users.noreply.github.com>
2019-12-12 11:55:00 -0500

    AOC Day 2, Part 2

--- src/adventofcode/2019/aoc.js
+++ src/adventofcode/2019/aoc.js
@@ -1,10 +1,19 @@
 const SCRIPTS = require('./scripts');
 const DATA = require('./data');
+const ACTION = process.argv[2];
 
-console.log(`Day 1:
-Sum of the fuel requirements for all of the modules on your spacecraft:  ${SCRIPTS.calculateFuel(DATA.SPACECRAFT_DATA['Module Masses'])}
-`);
 
-console.log(`Day 2, Part 1:
-Value of position [0] after the program halts:  ${SCRIPTS.resetGravityAssist(DATA.SPACECRAFT_DATA.Intcodes)}
+if (ACTION == 1) {
+	console.log(`Day 1:
+	Sum of the fuel requirements for all of the modules on your spacecraft:  ${SCRIPTS.calculateFuel(DATA.SPACECRAFT_DATA['Module Masses'])}
+	`);
+} else if (ACTION == 2) {
+
+	console.log(`Day 2, Part 1:
+	Value of position [0] after the program halts:  ${'reset', SCRIPTS.resetGravityAssist(DATA.SPACECRAFT_DATA.Intcodes)}
+	`)
+} else if (ACTION == 2.1) {
+	console.log(`Day 2, Part 2:
+	${'initiate', SCRIPTS.resetGravityAssist(DATA.SPACECRAFT_DATA.Intcodes)} produces output 19690720.
 `)
+}
--- src/adventofcode/2019/scripts.js
+++ src/adventofcode/2019/scripts.js
@@ -4,29 +4,50 @@ function calculateFuel(masses) {
 	}, 0)
 }
 
-function resetGravityAssist(opCodes) {
+function resetGravityAssist(opCodes, operation, noun = 12, verb = 2) {
 	(function restoreState() {
 		opCodes[1] = 12;
 		opCodes[2] = 2;
 	})();
 
-	for (let i = 0; i < opCodes.length; i += 4) {
-		const opCode = opCodes[i];
-		const position_1 = opCodes[opCodes[i + 1]];
-		const position_2 = opCodes[opCodes[i + 2]];
-		const outputIndex = opCodes[i + 3];
+	function reset() {
+		for (let i = 0; i < opCodes.length; i += 4) {
+			const opCode = opCodes[i];
+			const position_1 = opCodes[opCodes[i + 1]];
+			const position_2 = opCodes[opCodes[i + 2]];
+			const outputIndex = opCodes[i + 3];
 
-		if (opCode === 1) {
-			opCodes[outputIndex] = position_1 + position_2;
-		} else if (opCode === 2) {
-			opCodes[outputIndex] = position_1 * position_2;
-		} else if (opCode === 99) {
-			return opCodes[0];
-		} else {
-			console.error(`Invalid code:  ${opCode}`);
-			break;
+			if (opCode === 1) {
+				opCodes[outputIndex] = position_1 + position_2;
+			} else if (opCode === 2) {
+				opCodes[outputIndex] = position_1 * position_2;
+			} else if (opCode === 99) {
+				return opCodes[0];
+			} else {
+				console.error(`Invalid code:  ${opCode}`);
+			}
 		}
 	}
+
+	function initiate() {
+		let result;
+		opCodes.forEach(code => {
+			let output = code * noun + verb;
+			if (output === 19690720) {
+				result = code;
+			}
+		});
+		return result;
+	}
+
+	function getResults(params) {
+		if (operation == 'reset') {
+			reset();
+		} else if (operation == 'initiate') {
+			initiate();
+		}
+	}
+	getResults();
 }
 
 module.exports = {
