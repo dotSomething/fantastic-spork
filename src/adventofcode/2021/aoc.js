67d2b96896f03f076154a076aaed4dacd8419eed
dotSomething <17045972+dotSomething@users.noreply.github.com>
2021-12-02 22:42:44 -0500

    Day 2 - Dive

--- src/adventofcode/2021/aoc.js
+++ src/adventofcode/2021/aoc.js
@@ -2,13 +2,35 @@ const DATA = require('./data');
 
 function SonarSweepMap(array) {
     let counter = 0;
-    
+
     array.map((element, index) => {
-        if(element > array[index -1]) {
+        if (element > array[index - 1]) {
             counter++;
         }
     })
     return counter;
 }
 
-console.log(SonarSweepMap(DATA.sonarDepths));
+function Dive(array) {
+    let horizontal = 0;
+    let vertical = 0;
+    const { directions, values } = DATA.coordinates[0];
+
+    for (let index = 0; index < directions.length; index++) {
+        
+        if (directions[index] === 'x') {
+            horizontal = values[index] + horizontal;
+        }
+        if (directions[index] === 'a') {
+            vertical = values[index] + vertical;
+        }
+        if (directions[index] === 's') {
+            vertical = vertical - values[index] ;
+        }
+    }
+
+    return horizontal * vertical;
+
+}
+
+console.log(Dive(DATA.coordinates));
